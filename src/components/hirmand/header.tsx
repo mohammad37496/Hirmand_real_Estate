e2cb090836b862eccaddc58f29486d02b88191ec
mohammad37496 <138461435+mohammad37496@users.noreply.github.com>
2026-09-20 02:33:18 +0330

    fix(nav): provide explicit properties search defaults

--- src/components/hirmand/header.tsx
+++ src/components/hirmand/header.tsx
@@ -61,7 +61,7 @@ export function Header() {
               <Link
                 key={item.id}
                 to="/properties"
-                search={{}}
+                search={{ q: "", transactionType: "", propertyType: "", neighborhood: "" }}
                 className={cn(pathname === "/properties" && "is-current")}
                 onClick={closeMenu}
               >
