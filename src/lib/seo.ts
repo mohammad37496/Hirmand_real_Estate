1b62b54b616da6dde35fbc29207be87610976209
mohammad37496 <138461435+mohammad37496@users.noreply.github.com>
2026-09-25 02:41:03 +0330

    fix: escape javascript line separators in JSON-LD

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -28,8 +28,8 @@ export function safeJsonLd(value: unknown): string {
     .replace(/</g, "\\u003c")
     .replace(/>/g, "\\u003e")
     .replace(/&/g, "\\u0026")
-    .replace(/\\u2028/g, "\\u2028")
-    .replace(/\\u2029/g, "\\u2029");
+    .replace(/\u2028/g, "\\u2028")
+    .replace(/\u2029/g, "\\u2029");
 }
 
 export function absoluteUrl(path = "/"): string {
