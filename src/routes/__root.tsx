8e443a5dc9eef9ba159cc7ea1aa6b7078d635b4a
mohammad37496 <138461435+mohammad37496@users.noreply.github.com>
2026-09-24 12:34:17 +0330

    chore: load property detail visual layer

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,7 +9,7 @@ import themeProCss from "../theme-pro.css?url";
 import themeProPagesCss from "../theme-pro-pages.css?url";
 import mobileCss from "../mobile.css?url";
 import refinementsCss from "../refinements.css?url";
-import uiClarityCss from "../ui-clarity.css?url";
+import uiClarityCss from "../ui-clarity.css?url";\nimport propertyDetailProCss from "../property-detail-pro.css?url";
 
 export const Route = createRootRoute({
   head: () => ({
@@ -41,7 +41,7 @@ export const Route = createRootRoute({
       { rel: "stylesheet", href: themeProPagesCss },
       { rel: "stylesheet", href: mobileCss },
       { rel: "stylesheet", href: refinementsCss },
-      { rel: "stylesheet", href: uiClarityCss },
+      { rel: "stylesheet", href: uiClarityCss },\n      { rel: "stylesheet", href: propertyDetailProCss },
     ],
   }),
   component: RootDocument,
